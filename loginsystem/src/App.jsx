3d7af01d08df48296d9e8ff198217338e3ec4621
Erictse0302 <113747030+Erictse0302@users.noreply.github.com>
2023-10-25 11:36:46 +0800

    123
    21312

--- loginsystem/src/App.jsx
+++ loginsystem/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <LoginSignup/>
-        <AuthContent/>
+       <AuthContent/>
       </div>
     </>
   )
--- loginsystem/src/Components/LoginSingup/LoginSignup.jsx
+++ loginsystem/src/Components/LoginSingup/LoginSignup.jsx
@@ -1,39 +1,89 @@
 import React, { useState } from 'react'
 import './LoginSignup.css'
+import axios from 'axios';
 
 
 const LoginSignup = () => {
-    const[action,setAction] = useState("Sign Up")
+   
+    const [formData, setFormData] = useState({
+        lastName:'',
+        username: '',
+        password: '',
+        email: '',
+      });
+    
+      const [isRegistration, setIsRegistration] = useState(true);
 
+    
+      const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+
+
+      
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+        
+    try {
+        const endpoint = isRegistration ? '/api/signup' : '/api/login';
+        const response = await axios.post(`http://localhost:8080${endpoint}`, formData);
+        console.log(isRegistration ? 'Registration successful:' : 'Login successful:',response.data );
+        // Handle success, e.g., store authentication token in local storage
+      } catch (error) {
+        console.error(isRegistration ? 'Registration failed:' : 'Login failed:',response.data);
+        // Handle error, e.g., show an error message to the user
+        
+      } };
+       
+      
   return (
    <div className="container">
     <div className="header">
-        <div className="text">Sign up</div>
+        <div className="text">{isRegistration
+          ? 'Sign Up'
+          : 'Login In'}</div>
         <div className="underline"></div>
     </div>
+   <form onSubmit={handleSubmit}>
     <div className="inputs">
         <div className="input">
             <img src="" alt="" />
-            <input type="text" placeholder='Username'/>
+            <input type="text" placeholder='Username'  name = 'username'value={setFormData.username}
+            onChange={handleChange}/>
         </div>
-        {action==="Login"?<div></div>:
+        {isRegistration?
         <div className="input">
             <img src="" alt="" />
-            <input type="email" placeholder='Email' />
-        </div>}
+            <input type="text" placeholder='lastName' name='lastName'  value={setFormData.lastName}
+            onChange={handleChange} />
+        </div>:<div></div>}
+        {isRegistration?
         <div className="input">
             <img src="" alt="" />
-            <input type="password" placeholder='Password'/>
+            <input type="email" placeholder='Email' name='email'  value={setFormData.email}
+            onChange={handleChange} />
+        </div>:<div></div>}
+        <div className="input">
+            <img src="" alt="" />
+            <input type="password" placeholder='Password' name = 'password' value={setFormData.password}
+            onChange={handleChange} />
         </div>
     </div>
-    {action==="Sign Up"?<div></div>:
-    <div className="forgot-password">
-        Lost Password? <span>Click Here</span>
-    </div>}
-    <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
 
+
+    <div className="submit-container">
+    <button  className="submit-container" type="submit">{isRegistration ? 'Register' : 'Login'}</button>
+
+    </div>
+    </form>
+    <div className="forgot-password">
+    <p  onClick={() => setIsRegistration(!isRegistration)}>
+    {isRegistration
+          ? 'Already have an account? Click here to login.'
+          : 'Don\'t have an account? Click here to register.'}
+      </p>
+    
     </div>
    </div>
   )
